615d8cc4934a7da675037d80e1b3bb903f47c8d0
francois <francois@hhworkstation>
2015-04-27 18:36:33 +0200

    creation des projets et classe pour spring renomage des demos

---
+++ ocelot-spring/src/main/java/fr/hhdev/ocelot/spring/SpringResolver.java
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ */
+package fr.hhdev.ocelot.spring;
+
+import fr.hhdev.ocelot.spi.DataServiceException;
+import fr.hhdev.ocelot.spi.DataServiceResolverId;
+import fr.hhdev.ocelot.spi.DataServiceResolver;
+
+/**
+ * Resolver of POJO
+ * @author hhfrancois
+ */
+@DataServiceResolverId("SPRING")
+public class SpringResolver implements DataServiceResolver {
+
+	@Override
+	public Object resolveDataService(String dataService) throws DataServiceException {
+		try {
+			return Class.forName(dataService).newInstance();
+		} catch (ClassNotFoundException |InstantiationException | IllegalAccessException ex) {
+			throw new DataServiceException(dataService, ex);
+		}
+	}
+}
