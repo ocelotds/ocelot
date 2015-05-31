c2b39dc53d459d8d44a2ef45201267c84e03dd6e
hhfrancois <francois.achache@gmail.com>
2015-05-31 02:46:54 +0200

    add notice for complex cachekey

--- ocelot-processor/src/main/java/fr/hhdev/ocelot/annotations/JsCacheResult.java
+++ ocelot-processor/src/main/java/fr/hhdev/ocelot/annotations/JsCacheResult.java
@@ -22,5 +22,16 @@ public @interface JsCacheResult {
 	int minute() default 0;
 	int second() default 0;
 	int millisecond() default 0;
-	String keys() default "*";
+	/**
+	 * Rules for parameters about key for cache, separate by coma<br>
+	 * '-' : parameter is not used<br>
+	 * '*' : parameter is used fully<br>
+	 * '**' : parameters until the last is used fully<br>
+	 * 'id' : parameter.id is used<br>
+	 * <b>Example</b> : the method 'package1.class1.method1(Object o1, Object o2, Object o3, Object o4, Object o5)'<br>
+	 * is annotated with  @JsCacheResult(keys="-,*,name,**")<br>
+	 * so the cache key is compute from package1.class1.method1(o2,o3.name,o4,o5)<br>
+	 * @return 
+	 */
+	String keys() default "**";
 }
