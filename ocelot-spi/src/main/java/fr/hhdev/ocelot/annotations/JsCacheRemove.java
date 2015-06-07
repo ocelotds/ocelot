81e876cf5672d1616fd3c2d1ff91be02894583d6
hhfrancois <francois.achache@gmail.com>
2015-06-07 21:38:15 +0200

    annotations pour nettoyer les caches

---
+++ ocelot-spi/src/main/java/fr/hhdev/ocelot/annotations/JsCacheRemove.java
@@ -0,0 +1,41 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package fr.hhdev.ocelot.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.enterprise.util.Nonbinding;
+import javax.interceptor.InterceptorBinding;
+
+/**
+ *
+ * @author hhfrancois
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@InterceptorBinding
+public @interface JsCacheRemove {
+	@Nonbinding Class cls();
+	@Nonbinding String methodName();
+	/**
+	 * Define order keys, begin at 0, , separate by coma<br>
+	 * its the order of keys not the order of parameter for compute keys.
+	 * @return 
+	 */
+	@Nonbinding String orderKeys() default "";
+	/**
+	 * Rules for parameters about key for cache, separate by coma<br>
+	 * '-' : parameter is not used<br>
+	 * '*' : parameter is used fully<br>
+	 * '**' : parameters until the last is used fully<br>
+	 * 'id' : parameter.id is used<br>
+	 * <b>Example</b> : the method 'package1.class1.method1(Object o1, Object o2, Object o3, Object o4, Object o5)'<br>
+	 * is annotated with  @JsCacheResult(keys="-,*,name,**")<br>
+	 * so the cache key is compute from package1.class1.method1(o2,o3.name,o4,o5)<br>
+	 * @return 
+	 */
+	@Nonbinding String keys() default "**";
+}
