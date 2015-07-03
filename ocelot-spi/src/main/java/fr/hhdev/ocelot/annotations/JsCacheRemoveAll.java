6b034c7f5d89a0ba4c5bfea74e6772e0ac40ebe4
hhfrancois <francois.achache@gmail.com>
2015-07-03 03:01:50 +0200

    BROWSER to APPLICATION more clear

--- ocelot-spi/src/main/java/fr/hhdev/ocelot/annotations/JsCacheRemoveAll.java
+++ ocelot-spi/src/main/java/fr/hhdev/ocelot/annotations/JsCacheRemoveAll.java
@@ -15,5 +15,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface JsCacheRemoveAll {
-	JsCacheStore store() default JsCacheStore.BROWSER;
+	JsCacheStore store() default JsCacheStore.APPLICATION;
 }
