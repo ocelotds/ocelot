54a073e5bdf515b00d1a60db2847ee43c34caae0
hhfrancois <francois.achache@gmail.com>
2015-07-05 16:29:18 +0200

    minification

---
+++ ocelot-web/src/main/java/fr/hhdev/ocelot/web/JSServlet.java
@@ -0,0 +1,100 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ */
+package fr.hhdev.ocelot.web;
+
+import fr.hhdev.ocelot.Constants;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet to serve ocelot-core.js and ocelot-services.js
+ *
+ * @author hhfrancois
+ */
+@WebServlet(urlPatterns = {Constants.SLASH_OCELOT_CORE_JS, Constants.SLASH_OCELOT_SERVICES_JS})
+public class JSServlet extends HttpServlet {
+
+	/**
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType(Constants.JSTYPE);
+		String minify = request.getParameter(Constants.MINIFY_PARAMETER);
+		String resourceName = request.getServletPath();
+		String filename;
+		if (Constants.SLASH_OCELOT_SERVICES_JS.equals(resourceName)) {
+			filename = request.getServletContext().getInitParameter(Constants.OCELOT_SERVICES_MIN);
+			if (Constants.FALSE.equalsIgnoreCase(minify)) {
+				filename = request.getServletContext().getInitParameter(Constants.OCELOT_SERVICES);
+			}
+		} else {
+			filename = request.getServletContext().getInitParameter(Constants.OCELOT_CORE_MIN);
+			if (Constants.FALSE.equalsIgnoreCase(minify)) {
+				filename = request.getServletContext().getInitParameter(Constants.OCELOT_CORE);
+			}
+		}
+		try (Writer out = response.getWriter()) {
+			try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
+				String inputLine;
+				while ((inputLine = in.readLine()) != null) {
+					out.write(inputLine);
+					out.write(Constants.BACKSLASH_N);
+				}
+			}
+		}
+	}
+
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			  throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			  throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
+
+}
