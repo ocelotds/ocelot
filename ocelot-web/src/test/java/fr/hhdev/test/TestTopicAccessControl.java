b21df1737884a7a953910ea84110681d808e916a
hhfrancois <francois.achache@gmail.com>
2015-07-15 22:28:45 +0200

    TopicAccessControl for topic security test Class

---
+++ ocelot-web/src/test/java/fr/hhdev/test/TestTopicAccessControl.java
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package fr.hhdev.test;
+
+import fr.hhdev.ocelot.security.TopicAccessControl;
+import javax.inject.Singleton;
+import javax.websocket.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author hhfrancois
+ */
+@Singleton
+public class TestTopicAccessControl implements TopicAccessControl {
+	
+	private final static Logger logger = LoggerFactory.getLogger(TestTopicAccessControl.class);
+
+	private boolean access = true; 
+
+	public boolean isAccess() {
+		return access;
+	}
+
+	public void setAccess(boolean access) {
+		this.access = access;
+	}
+
+	@Override
+	public void checkAccess(Session session, String topic) throws IllegalAccessException {
+		logger.debug("Check access to topic {} : access = {}", topic, access);
+		if(!access) {
+			throw new IllegalAccessException("access is setto false");
+		}
+	}
+	
+}
