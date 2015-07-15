676a5084b4e1b03c7d5ba6a9957af9fb6b40066f
hhfrancois <francois.achache@gmail.com>
2015-07-15 22:28:59 +0200

    move sessioManager in core

--- ocelot-web/src/main/java/fr/hhdev/ocelot/core/SessionManager.java
+++ ocelot-web/src/main/java/fr/hhdev/ocelot/core/SessionManager.java
@@ -2,13 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
  */
-package fr.hhdev.ocelot.web;
+package fr.hhdev.ocelot.core;
 
+import fr.hhdev.ocelot.security.TopicAccessControl;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.websocket.Session;
 import org.slf4j.Logger;
@@ -16,13 +19,18 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Singleton sessions manager
+ *
  * @author hhfrancois
  */
 @Singleton
 public class SessionManager {
+
 	private final static Logger logger = LoggerFactory.getLogger(SessionManager.class);
 	private final Map<String, Collection<Session>> sessionsByTopic = new HashMap<>();
-	
+
+	@Inject
+	Instance<TopicAccessControl> accessControls;
+
 	/**
 	 * Register session for topic
 	 *
@@ -30,14 +38,23 @@ public class SessionManager {
 	 * @param session
 	 */
 	public void registerTopicSession(String topic, Session session) {
+		try {
+			TopicAccessControl accessControl = accessControls.get();
+			accessControl.checkAccess(session, topic);
+		} catch (IllegalAccessException ex) {
+			logger.error("Subscribtion request to " + topic + " for " + session.getUserPrincipal() + " is illegal : '"+ex.getMessage()+"'");
+			return;
+		} catch (Exception ex) {
+			logger.warn("No topic access control found in project, add {} implementation in your project for add topic security.", TopicAccessControl.class);
+		}
 		Collection<Session> sessions;
 		if (sessionsByTopic.containsKey(topic)) {
 			sessions = sessionsByTopic.get(topic);
 		} else {
-			sessions =  Collections.synchronizedCollection(new ArrayList<Session>());
+			sessions = Collections.synchronizedCollection(new ArrayList<Session>());
 			sessionsByTopic.put(topic, sessions);
 		}
-		logger.debug("'{}' subscribe to '{}'", session.getId(), topic);
+		logger.info("'{}' subscribe to '{}'", session.getId(), topic);
 		sessions.add(session);
 	}
 
@@ -74,7 +91,7 @@ public class SessionManager {
 	 */
 	public Collection<Session> getSessionsForTopic(String topic) {
 		Collection<Session> result;
-		if(existsSessionForTopic(topic)) {
+		if (existsSessionForTopic(topic)) {
 			return sessionsByTopic.get(topic);
 		} else {
 			result = Collections.EMPTY_LIST;
