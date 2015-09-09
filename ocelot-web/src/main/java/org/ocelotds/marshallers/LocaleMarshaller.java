d48bb0d39d04281c5b30e5374213b852201214ea
hhfrancois <francois.achache@gmail.com>
2015-09-09 23:55:52 +0200

    for use native Locale, implementsJsonMarshaller/unmarshaller

---
+++ ocelot-web/src/main/java/org/ocelotds/marshallers/LocaleMarshaller.java
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.ocelotds.marshallers;
+
+import java.util.Locale;
+import org.ocelotds.Constants;
+import org.ocelotds.marshalling.JsonMarshaller;
+import org.ocelotds.marshalling.exceptions.JsonMarshallingException;
+
+/**
+ *
+ * @author hhfrancois
+ */
+public class LocaleMarshaller implements JsonMarshaller<Locale> {
+
+	/**
+	 * 
+	 * @param obj
+	 * @return
+	 * @throws JsonMarshallingException 
+	 */
+	@Override
+	public String toJson(Locale obj) throws JsonMarshallingException {
+		return String.format("{\"%s\":\"%s\",\"%s\":\"%s\"}", Constants.Message.COUNTRY, obj.getCountry(), Constants.Message.LANGUAGE, obj.getLanguage());
+	}
+
+}
---
+++ ocelot-web/src/main/java/org/ocelotds/marshallers/LocaleUnmarshaller.java
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.ocelotds.marshallers;
+
+import java.io.StringReader;
+import java.util.Locale;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import org.ocelotds.Constants;
+import org.ocelotds.marshalling.JsonUnmarshaller;
+import org.ocelotds.marshalling.exceptions.JsonUnmarshallingException;
+
+/**
+ * For change to runtime an object received from clientto an other java type<br>
+ * Use @JsonUnmarshaller annotation on argument to specify the unmarshaller that implements JsonUnmarshaller
+ * @author hhfrancois
+ */
+public class LocaleUnmarshaller implements JsonUnmarshaller<Locale> {
+
+	/**
+	 * 
+	 * @param json
+	 * @return
+	 * @throws JsonUnmarshallingException 
+	 */
+	@Override
+	public Locale toJava(String json) throws JsonUnmarshallingException {
+		Locale locale;
+		try (JsonReader reader = Json.createReader(new StringReader(json))) {
+			JsonObject root = reader.readObject();
+			locale = new Locale(root.getString(Constants.Message.LANGUAGE), root.getString(Constants.Message.COUNTRY));
+		}
+		return locale;
+	}
+	
+}
