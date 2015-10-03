48065f76336bce2efef90489c03db227c3854980
hhfrancois <francois.achache@gmail.com>
2015-10-03 21:00:48 +0200

    wildfly security

--- ocelot-web/src/main/java/org/ocelotds/security/containers/WildflySubjectServices.java
+++ ocelot-web/src/main/java/org/ocelotds/security/containers/WildflySubjectServices.java
@@ -3,11 +3,18 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package org.ocelotds.security.containers;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import org.jboss.security.identity.Identity;
 import java.security.Principal;
+import java.security.acl.Group;
+import java.util.Set;
 import javax.inject.Inject;
 import javax.security.auth.Subject;
+import org.jboss.security.SecurityContext;
+import org.jboss.security.SecurityContextAssociation;
+import org.jboss.security.SecurityContextUtil;
+import org.jboss.security.identity.Role;
+import org.jboss.security.identity.extensions.CredentialIdentityFactory;
+import org.jboss.security.identity.plugins.SimpleRoleGroup;
 import org.ocelotds.Constants;
 import org.ocelotds.logger.OcelotLogger;
 import org.ocelotds.annotations.ContainerQualifier;
@@ -19,44 +26,84 @@ import org.slf4j.Logger;
  */
 @ContainerQualifier(Constants.Container.WILDFLY)
 public class WildflySubjectServices implements ContainerSubjectServices {
-	
-	private static final String SECURITY_CONTEXT_ASSOCIATION = "org.jboss.security.SecurityContextAssociation";
-	private static final String IDENTITY_FACTORY = "org.jboss.security.identity.extensions.CredentialIdentityFactory";
-	private static final String IDENTITY = "org.jboss.security.identity.Identity";
-	private static final String SECURITY_CONTEXT = "org.jboss.security.SecurityContext";
 
 	@Inject
 	@OcelotLogger
 	private Logger logger;
 
+	/**
+	 *
+	 * @return
+	 */
 	@Override
-	public Subject getSubject() throws Exception {
-		Class<?> secuCtxClass = Class.forName(SECURITY_CONTEXT_ASSOCIATION);
-		Method getSubject = secuCtxClass.getMethod("getSubject");
-		Subject subject = (Subject) getSubject.invoke(null);
-		logger.debug("Use wildfly implementation for get security context");
-		return subject;
+	public org.ocelotds.security.SecurityContext getSecurityContext() {
+		return new WildflySecurityContext(SecurityContextAssociation.getPrincipal(), SecurityContextAssociation.getSubject(), SecurityContextAssociation.getCredential());
 	}
 
+	/**
+	 *
+	 * @param securityContext
+	 */
 	@Override
-	public void setSubject(Subject subject, Principal principal) throws Exception {
-		Class<?> identityClass = Class.forName(IDENTITY);
-		Class<?> secuCtxAssClass = Class.forName(SECURITY_CONTEXT_ASSOCIATION);
-		Class<?> factoryIdentityClass = Class.forName(IDENTITY_FACTORY);
-		Class<?> secuCtxClass = Class.forName(SECURITY_CONTEXT);
-
-		Method getSecurityContext = secuCtxAssClass.getMethod("getSecurityContext");
-		Object securityContext = getSecurityContext.invoke(null);
-
-		Method getUtil = secuCtxClass.getMethod("getUtil");
-		Object util = getUtil.invoke(securityContext);
-
-		Method createSubjectInfo = util.getClass().getMethod("createSubjectInfo", identityClass, Subject.class);
-		Method getIdentity = factoryIdentityClass.getMethod("getIdentity", Principal.class, Object.class);
-
-		Object identity = getIdentity.invoke(null, principal, null);
-		createSubjectInfo.invoke(util, identity, subject);
-		logger.debug("Use wildfly implementation for set security context");
+	public void setSecurityContext(org.ocelotds.security.SecurityContext securityContext) {
+		final WildflySecurityContext context = (WildflySecurityContext) securityContext;
+		final SecurityContext secuContext = SecurityContextAssociation.getSecurityContext();
+		final Role roleGroup = getRoleGroup(context.getSubject());
+		final Identity identity = CredentialIdentityFactory.createIdentity(context.getPrincipal(), context.getCredential(), roleGroup);
+		if (null != secuContext) {
+			SecurityContextUtil util = secuContext.getUtil();
+			util.createSubjectInfo(identity, context.getSubject());
+		}
 	}
-	
+
+	/**
+	 * Compute Role
+	 *
+	 * @param subject
+	 * @return
+	 */
+	public static Role getRoleGroup(final Subject subject) {
+		final Set<Group> groups = subject.getPrincipals(Group.class);
+		for (Group group : groups) {
+			if ("Roles".equals(group.getName())) {
+				return new SimpleRoleGroup(group);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Private implementation for wildfly
+	 */
+	static class WildflySecurityContext implements org.ocelotds.security.SecurityContext {
+
+		private final Principal principal;
+		private final Subject subject;
+		private final Object credential;
+
+		public WildflySecurityContext(Principal principal, Subject subject, Object credential) {
+			this.principal = principal;
+			this.subject = subject;
+			this.credential = credential;
+		}
+
+		@Override
+		public Principal getPrincipal() {
+			return principal;
+		}
+
+		@Override
+		public Subject getSubject() {
+			return subject;
+		}
+
+		public Object getCredential() {
+			return credential;
+		}
+
+		@Override
+		public String toString() {
+			return "{\"principal\":" + principal + ",\"subject\":" + subject + ",\"credential\":" + credential + "}";
+		}
+	}
 }
