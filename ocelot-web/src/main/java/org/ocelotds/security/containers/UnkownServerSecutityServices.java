7c4c6e9c3e5e6c9b000c733b2d395bb9b8752e00
hhfrancois <francois.achache@gmail.com>
2015-10-18 17:39:21 +0200

    remove qualifier for default implementation

--- ocelot-web/src/main/java/org/ocelotds/security/containers/UnkownServerSecutityServices.java
+++ ocelot-web/src/main/java/org/ocelotds/security/containers/UnkownServerSecutityServices.java
@@ -3,15 +3,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package org.ocelotds.security.containers;
 
-import org.ocelotds.Constants;
-import org.ocelotds.annotations.ContainerQualifier;
 import org.ocelotds.security.SecurityContext;
 
 /**
  *
  * @author hhfrancois
  */
-@ContainerQualifier(Constants.Container.UNKNOWN)
 public class UnkownServerSecutityServices implements ContainerSecurityServices {
 
 	@Override
