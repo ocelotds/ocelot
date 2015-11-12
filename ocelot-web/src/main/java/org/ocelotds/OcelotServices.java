af1e12d1774392f96827d5e03b245d0b58828625
hhfrancois <francois.achache@gmail.com>
2015-11-12 22:38:42 +0100

    remove Session Injection in method

--- ocelot-web/src/main/java/org/ocelotds/OcelotServices.java
+++ ocelot-web/src/main/java/org/ocelotds/OcelotServices.java
@@ -3,13 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package org.ocelotds;
 
-import java.security.Principal;
-import org.ocelotds.context.ThreadLocalContextHolder;
 import org.ocelotds.annotations.DataService;
 import org.ocelotds.annotations.JsCacheRemove;
 import org.ocelotds.annotations.JsCacheResult;
-import org.ocelotds.annotations.TransientDataService;
-import org.ocelotds.core.MethodWithSessionInjection;
 import org.ocelotds.core.SessionManager;
 import org.ocelotds.core.UpdatedCacheManager;
 import java.util.Collection;
@@ -20,6 +16,7 @@ import javax.websocket.Session;
 import org.ocelotds.annotations.JsTopic;
 import org.ocelotds.annotations.JsTopicName;
 import org.ocelotds.annotations.OcelotLogger;
+import org.ocelotds.context.OcelotContext;
 import org.ocelotds.marshallers.LocaleMarshaller;
 import org.ocelotds.marshallers.LocaleUnmarshaller;
 import org.ocelotds.marshalling.annotations.JsonMarshaller;
@@ -43,21 +40,20 @@ public class OcelotServices {
 	@Inject
 	private SessionManager sessionManager;
 
-	@MethodWithSessionInjection
-	public void setLocale(@JsonUnmarshaller(LocaleUnmarshaller.class) Locale locale) {
-		ThreadLocalContextHolder.put(Constants.LOCALE, locale);
-	}
+	@Inject
+	private OcelotContext ocelotContext;
+
+	@Inject
+	private Session session;
 
 	/**
 	 * define locale for current user
 	 *
 	 * @param locale
-	 * @param session
 	 */
-	@TransientDataService
 	@JsCacheRemove(cls = OcelotServices.class, methodName = "getLocale", keys = {})
-	public void setLocale(@JsonUnmarshaller(LocaleUnmarshaller.class) Locale locale, Session session) {
-		session.getUserProperties().put(Constants.LOCALE, locale);
+	public void setLocale(@JsonUnmarshaller(LocaleUnmarshaller.class) Locale locale) {
+		ocelotContext.setLocale(locale);
 	}
 
 	/**
@@ -69,56 +65,29 @@ public class OcelotServices {
 	@JsonMarshaller(LocaleMarshaller.class)
 	public Locale getLocale() {
 		logger.debug("Receive getLocale call from client.");
-		return (Locale) ThreadLocalContextHolder.get(Constants.LOCALE);
-	}
-
-	/**
-	 * get current user locale
-	 *
-	 * @return
-	 */
-	@MethodWithSessionInjection
-	public String getUsername() {
-		return null;
+		return ocelotContext.getLocale();
 	}
 
 	/**
 	 * return current username from session
 	 *
-	 * @param session
 	 * @return
 	 */
-	@TransientDataService
-	public String getUsername(Session session) {
-		logger.debug("Receive getUsername call from client.");
-		Principal p = session.getUserPrincipal();
-		if(null!= p) {
-			return (String) session.getUserPrincipal().getName();
-		}
-		return Constants.ANONYMOUS;
+	public String getUsername() {
+		return ocelotContext.getUsername();
 	}
 
 	public Collection<String> getOutDatedCache(Map<String, Long> states) {
 		return updatedCacheManager.getOutDatedCache(states);
 	}
 
-	@MethodWithSessionInjection
-	public void subscribe(String topic) {
-	}
-
-	@MethodWithSessionInjection
-	public void unsubscribe(String topic) {
-	}
-
 	@JsTopic
-	@TransientDataService
-	public Integer subscribe(Session session, @JsTopicName(prefix = Constants.Topic.SUBSCRIBERS) String topic) throws IllegalAccessException {
+	public Integer subscribe(@JsTopicName(prefix = Constants.Topic.SUBSCRIBERS) String topic) throws IllegalAccessException {
 		return sessionManager.registerTopicSession(topic, session);
 	}
 
 	@JsTopic
-	@TransientDataService
-	public Integer unsubscribe(Session session, @JsTopicName(prefix = Constants.Topic.SUBSCRIBERS) String topic) {
+	public Integer unsubscribe(@JsTopicName(prefix = Constants.Topic.SUBSCRIBERS) String topic) {
 		return sessionManager.unregisterTopicSession(topic, session);
 	}
 
