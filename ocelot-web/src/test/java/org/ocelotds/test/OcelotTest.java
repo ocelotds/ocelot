b3a796467cc8002e3be6b42e9c9579e4aef9feb0
hhfrancois <francois.achache@gmail.com>
2015-11-16 01:51:27 +0100

    add version for arquillian test

--- ocelot-web/src/test/java/org/ocelotds/test/OcelotTest.java
+++ ocelot-web/src/test/java/org/ocelotds/test/OcelotTest.java
@@ -163,7 +163,7 @@ public class OcelotTest {
 	 * @param webArchive
 	 */
 	public static void addOcelotJar(WebArchive webArchive) {
-		String version = "[2,)";
+		String version = "2.4.6-SNAPSHOT";
 		File[] imports = Maven.resolver().resolve("org.ocelotds:ocelot-web:"+version, "org.ocelotds:ocelot-core:"+version).withTransitivity().asFile();
 		webArchive.addAsLibraries(imports);
 	}
