9693675aa8cb67b3fd8ce426ae699e5d3db768b6
hhfrancois <francois.achache@gmail.com>
2015-12-16 01:46:16 +0100

    fix bug, improve find fine method improve test

--- ocelot-web/src/main/java/org/ocelotds/core/services/CallServiceManager.java
+++ ocelot-web/src/main/java/org/ocelotds/core/services/CallServiceManager.java
@@ -22,8 +22,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
