5ded27a1abf5bb5885e57117cf8b837e02b42cd5
hhfrancois <francois.achache@gmail.com>
2015-12-16 23:57:17 +0100

    increase version

--- ocelot-web/src/test/java/org/ocelotds/test/OcelotTest.java
+++ ocelot-web/src/test/java/org/ocelotds/test/OcelotTest.java
@@ -159,7 +159,7 @@ public class OcelotTest {
 	 * @param webArchive
 	 */
 	public static void addOcelotJar(WebArchive webArchive) {
-		String version = "2.6.4-SNAPSHOT";
+		String version = "2.6.5-SNAPSHOT";
 		File[] imports = Maven.resolver().resolve("org.ocelotds:ocelot-web:" + version, "org.ocelotds:ocelot-core:" + version).withTransitivity().asFile();
 		webArchive.addAsLibraries(imports);
 	}
