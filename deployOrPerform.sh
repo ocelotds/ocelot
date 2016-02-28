5dd22be160b6862625335c1c675b316c9499aacb
hhfrancois <francois.achache@gmail.com>
2016-02-28 14:08:52 +0100

    nop

--- deployOrPerform.sh
+++ deployOrPerform.sh
@@ -1,12 +1,9 @@
 CURRENT_VERSION=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text)"`
 echo "CURRENT_VERSION = "$CURRENT_VERSION
 echo "releaseVersion = "$releaseVersion
-env:
-  - PERFORM=false
 PERFORM=false
 if [ "$CURRENT_VERSION" = "$releaseVersion-SNAPSHOT" ]; then
-env:
-  - PERFORM=true
+PERFORM=true
 fi
 echo "PERFORM = "$PERFORM
 TAG_NAME=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}artifactId').text)"`-$releaseVersion
