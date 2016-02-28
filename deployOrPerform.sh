855aa5c0623a73338145f383128d5f2cb3af788d
hhfrancois <francois.achache@gmail.com>
2016-02-28 13:55:26 +0100

    tagname

--- deployOrPerform.sh
+++ deployOrPerform.sh
@@ -6,8 +6,8 @@ if [ "$CURRENT_VERSION" = "$releaseVersion-SNAPSHOT" ]; then
 PERFORM=true
 fi
 echo "PERFORM = "$PERFORM
-PROJECT_NAME=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}artifactId').text)"`
-echo "PROJECT_NAME = "$PROJECT_NAME
+TAG_NAME=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}artifactId').text)"`$releaseVersion
+echo "TAG_NAME = "$TAG_NAME
 # mvn --B -Dtag=$PROJECT_NAME-$releaseVersion release:prepare \
 #                 -DreleaseVersion=$releaseVersion \
 #                 -DdevelopmentVersion=$developmentVersion
