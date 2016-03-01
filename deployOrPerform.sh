18778a624e4434981357bf26ceb229dd263fbe1d
hhfrancois <francois.achache@gmail.com>
2016-03-01 22:21:06 +0100

    [ci skip]

--- deployOrPerform.sh
+++ deployOrPerform.sh
@@ -1,7 +1,18 @@
-if [ "$TRAVIS_BRANCH " = "release" ]; then
+if [ "$TRAVIS_BRANCH " = "master" ]; then
+CURRENT_VERSION=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text)"`
+tag=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}artifactId').text)"`-$releaseVersion
+echo "CURRENT_VERSION = "$CURRENT_VERSION
+echo "tag = "$tag
+echo "releaseVersion = "$releaseVersion
+echo "developmentVersion = "$developmentVersion
+if [ "$CURRENT_VERSION" = "$releaseVersion-SNAPSHOT" ]; then
+echo "PERFORM "$releaseVersion
+git checkout master
 git config --global user.email "ocelotds.francois@gmail.com"
 git config --global user.name "Travis-CI"
-mvn --B release:clean release:prepare release:perform --settings .travis-settings.xml
+mvn --B release:clean release:prepare release:perform -Dtag=$tag-$releaseVersion -DreleaseVersion=$releaseVersion -DdevelopmentVersion=$developmentVersion --settings .travis-settings.xml
 else
+echo "DEPLOY "$CURRENT_VERSION
 mvn deploy --settings .travis-settings.xml
+fi
 fi
