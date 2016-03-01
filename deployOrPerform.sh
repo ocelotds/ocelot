4e4d590ee249c5656dd38565afd4a759a6d0ea6c
hhfrancois <francois.achache@gmail.com>
2016-03-01 22:28:35 +0100

    typo

--- deployOrPerform.sh
+++ deployOrPerform.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_BRANCH " = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
 CURRENT_VERSION=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text)"`
 tag=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}artifactId').text)"`-$releaseVersion
 echo "CURRENT_VERSION = "$CURRENT_VERSION
