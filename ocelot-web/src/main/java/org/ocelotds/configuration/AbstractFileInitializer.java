7301353c1e597476a4def3d665d37ee6902e3970
hhfrancois <francois.achache@gmail.com>
2016-03-08 22:37:32 +0100

    remove CTXPATH generation

--- ocelot-web/src/main/java/org/ocelotds/configuration/AbstractFileInitializer.java
+++ ocelot-web/src/main/java/org/ocelotds/configuration/AbstractFileInitializer.java
@@ -4,20 +4,33 @@
 package org.ocelotds.configuration;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
+import javax.inject.Inject;
+import org.ocelotds.Constants;
+import org.ocelotds.annotations.OcelotLogger;
+import static org.ocelotds.configuration.JsFileInitializer.OCELOT_CORE_RESOURCE;
+import org.slf4j.Logger;
 
 /**
  *
  * @author hhfrancois
  */
 public abstract class AbstractFileInitializer {
+
+	@Inject
+	@OcelotLogger
+	private Logger logger;
+
 	/**
 	 * Remove file on undeploy application
 	 *
 	 * @param filename
-	 * @return 
+	 * @return
 	 */
-	protected boolean deleteFile(String filename) {
+	boolean deleteFile(String filename) {
 		if (null != filename && !filename.isEmpty()) {
 			File file = new File(filename);
 			if (file.exists()) {
@@ -26,8 +39,26 @@ public abstract class AbstractFileInitializer {
 		}
 		return false;
 	}
-	
+
 	URL getContentURL(String resourcename) {
 		return AbstractFileInitializer.class.getResource(resourcename);
+	}
+
+	boolean writeStreamToOutputStream(InputStream input, OutputStream out) {
+		if (null != input && null != out) {
+			try (InputStream in = input) {
+				byte[] buffer = new byte[Constants.DEFAULT_BUFFER_SIZE];
+				int n = 0;
+				while (-1 != (n = in.read(buffer))) {
+					out.write(buffer, 0, n);
+				}
+				return true;
+			} catch (IOException ex) {
+				logger.error("Generation of '" + OCELOT_CORE_RESOURCE + "' failed.", ex);
+			}
+		} else {
+			logger.warn("Generation of '{}' failed. File not found", OCELOT_CORE_RESOURCE);
+		}
+		return false;
 	}
 }
