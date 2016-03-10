2875a3bcd7624191dc2ce8ee478ef3ebac1ad975
hhfrancois <francois.achache@gmail.com>
2016-03-10 21:43:30 +0100

    improve multicall test

---
+++ ocelot-int-glassfish/src/test/java/org/ocelotds/objects/ResultMonitored.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ocelotds.objects;
+
+/**
+ *
+ * @author hhfrancois
+ */
+public class ResultMonitored<T> {
+	T result;
+	long time = 0L;
+	int num = 0;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public ResultMonitored(T result, int num) {
+		this.result = result;
+		this.num = num;
+	}
+
+	public T getResult() {
+		return result;
+	}
+
+	public void setResult(T result) {
+		this.result = result;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+	
+	
+	
+	
+	
+}
