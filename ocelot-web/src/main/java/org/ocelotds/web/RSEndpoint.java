b9f4454e9915eccd684ad7643212ddeacf359cc7
hhfrancois <francois.achache@gmail.com>
2016-03-27 21:53:30 +0200

    remove monitor from endpoint

--- ocelot-web/src/main/java/org/ocelotds/web/RSEndpoint.java
+++ ocelot-web/src/main/java/org/ocelotds/web/RSEndpoint.java
@@ -13,10 +13,8 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import org.ocelotds.Constants;
 import org.ocelotds.annotations.OcelotLogger;
@@ -49,22 +47,20 @@ public class RSEndpoint {
 	 * Retrieves representation of an instance of org.ocelotds.GenericResource
 	 *
 	 * @param json
-	 * @param monitor
 	 * @return an instance of java.lang.String
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public String getMessageToClient(@FormParam(Constants.Message.MFC) String json, @DefaultValue("false") @QueryParam(Constants.Options.MONITOR) boolean monitor) {
+	public String getMessageToClient(@FormParam(Constants.Message.MFC) String json) {
 		HttpSession httpSession = request.getSession();
-		setContext(httpSession, monitor);
+		setContext(httpSession);
 		MessageFromClient message = MessageFromClient.createFromJson(json);
 		MessageToClient createMessageToClient = messageToClientService.createMessageToClient(message, httpSession);
 		return createMessageToClient.toJson();
 	}
 
-	void setContext(HttpSession httpSession, boolean monitor) {
-		httpSession.setAttribute(Constants.Options.MONITOR, monitor);
+	void setContext(HttpSession httpSession) {
 		ThreadLocalContextHolder.put(Constants.HTTPREQUEST, request);
 		ThreadLocalContextHolder.put(Constants.HTTPSESSION, httpSession);
 	}
