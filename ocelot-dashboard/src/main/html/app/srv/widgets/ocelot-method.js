4edb4bc2f72d30ef7aca096b87556559c95f352b
hhfrancois <francois.achache@gmail.com>
2016-05-25 09:47:31 +0200

    refacto jpapa

--- ocelot-dashboard/src/main/html/app/srv/widgets/ocelot-method.js
+++ ocelot-dashboard/src/main/html/app/srv/widgets/ocelot-method.js
@@ -19,7 +19,7 @@
 		vm.srvname = $scope.srvname;
 		vm.method = $scope.method;
 		vm.args = [];
-		vm.help = null;
+		vm.help = buildhelp();
 		vm.method.argtemplates.forEach(function (argtemplate, idx, array) {
 			vm.args.push(argtemplate);
 		});
@@ -31,31 +31,35 @@
 				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 			]
 		};
-		vm.buildhelp = function () {
-			if (!vm.help) {
-				vm.help = this.srvname + "." + this.method.name + "(";
-				vm.method.argtemplates.forEach(function (argtemplate, idx, array) {
-					vm.help += argtemplate;
-					vm.help += (idx < array.length - 1) ? ", " : "";
-				});
-				vm.help += ")";
-				vm.help += "\n\t.event(function(evt:OcelotEvent) {\n\t\tif(evt.type===\"RESULT\") {\n\t\t\t// OK\n\t\t} else {\n\t\t\t// FAIL\n\t\t}\n\t})";
-				vm.help += "\n\t.then(function(result:Object) {\n\t\t// OK\n\t})";
-				vm.help += "\n\t.catch(function(fault:Fault) {\n\t\t// FAIL\n\t});";
-			}
-		};
-		vm.buildhelp();
 		vm.result = vm.help;
-		vm.prevent = function (event) {
+		vm.prevent = prevent;
+		vm.clear = clear;
+		vm.send = send;
+		
+		
+		// =============================================
+		function buildhelp() {
+			var help = vm.srvname + "." + vm.method.name + "(";
+			vm.method.argtemplates.forEach(function (argtemplate, idx, array) {
+				help += argtemplate;
+				help += (idx < array.length - 1) ? ", " : "";
+			});
+			help += ")";
+			help += "\n\t.event(function(evt:OcelotEvent) {\n\t\tif(evt.type===\"RESULT\") {\n\t\t\t// OK\n\t\t} else {\n\t\t\t// FAIL\n\t\t}\n\t})";
+			help += "\n\t.then(function(result:Object) {\n\t\t// OK\n\t})";
+			help += "\n\t.catch(function(fault:Fault) {\n\t\t// FAIL\n\t});";
+			return help;
+		}
+		function prevent(event) {
 			event.preventDefault();
 			event.stopPropagation();
-		};
-		vm.clear = function (event) {
+		}
+		function clear() {
 			vm.result = vm.help;
 			vm.chart.data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-		};
-		vm.send = function (event) {
-			var methodcall = this.srvname + "." + this.method.name + "(";
+		}
+		function send() {
+			var methodcall = vm.srvname + "." + vm.method.name + "(";
 			vm.args.forEach(function (arg, idx, array) {
 				methodcall += arg;
 				methodcall += (idx < array.length - 1) ? ", " : "";
@@ -64,12 +68,8 @@
 			var processtime = ".event(function(evt) {vm.chart.data[0].shift();vm.chart.data[0].push(evt.totaltime);vm.chart.data[1].shift();vm.chart.data[1].push(evt.javatime);$scope.$apply();})";
 			var processresult = ".then(function(result) {vm.result = JSON.stringify(result, null, 3);$scope.$apply();})";
 			var processerror = ".catch(function(fault) {vm.result = JSON.stringify(fault, null, 3);$scope.$apply();})";
-//		vm.chart.data[0].shift();
-//		vm.chart.data[0].push(Math.random());
-//		vm.chart.data[1].shift();
-//		vm.chart.data[1].push(Math.random());
 			eval(methodcall + processtime + processresult + processerror + ";");
-		};
+		}
 	}
 })();
 
