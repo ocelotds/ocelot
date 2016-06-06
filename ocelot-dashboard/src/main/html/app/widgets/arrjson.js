43755ae3c2e26e485df0bdc4fb79de706f62b227
hhfrancois <francois.achache@gmail.com>
2016-06-07 01:15:17 +0200

    closure

--- ocelot-dashboard/src/main/html/app/widgets/arrjson.js
+++ ocelot-dashboard/src/main/html/app/widgets/arrjson.js
@@ -1,10 +1,14 @@
-angular.module('app.dashboard').filter('arrjson', function () {
-	return function (input) {
-		var result = [];
-		input.forEach(function (a, idx, arr) {
-			result.push(JSON.stringify(a));
-		});
-		return result.join(',');
-	};
-});
-
+(function () {
+	'use strict';
+	angular.module('app.dashboard').filter('arrjson', function () {
+		return function (input) {
+			var result = [];
+			if(input) {
+				input.forEach(function (a, idx, arr) {
+					result.push(JSON.stringify(a));
+				});
+			}
+			return result.join(',');
+		};
+	});
+})();
