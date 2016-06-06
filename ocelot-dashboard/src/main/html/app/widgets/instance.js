66efaff57f4bd646b1aa0092669665fc3857d84b
hhfrancois <francois.achache@gmail.com>
2016-06-07 01:15:27 +0200

    closure

--- ocelot-dashboard/src/main/html/app/widgets/instance.js
+++ ocelot-dashboard/src/main/html/app/widgets/instance.js
@@ -1,7 +1,10 @@
-angular.module('app.dashboard').filter('instance', function () {
-	return function (ds) {
-		var instance = ds.substring(ds.lastIndexOf(".")+1);
-		return instance.substr(0, 1).toLowerCase()+instance.substring(1);
-	};
-});
+(function () {
+	'use strict';
+	angular.module('app.dashboard').filter('instance', function () {
+		return function (ds) {
+			var instance = ds.substring(ds.lastIndexOf(".")+1);
+			return instance.substr(0, 1).toLowerCase()+instance.substring(1);
+		};
+	});
+})();
 
