78274befde41c0d0bf8dc8991b279648f176f11d
hhfrancois <francois.achache@gmail.com>
2016-06-07 00:58:07 +0200

    resource /ocelot/core

--- ocelot-web/src/main/java/org/ocelotds/web/rest/RsJsCoreUrl.java
+++ ocelot-web/src/main/java/org/ocelotds/web/rest/RsJsCoreUrl.java
@@ -4,19 +4,11 @@
 package org.ocelotds.web.rest;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-import org.ocelotds.Constants;
-import org.ocelotds.OcelotServices;
 import org.ocelotds.annotations.OcelotResource;
 
 /**
@@ -28,17 +20,13 @@ import org.ocelotds.annotations.OcelotResource;
 @RequestScoped
 @OcelotResource
 public class RsJsCoreUrl {
-	@Context
-	private UriInfo context;
-	
 	/**
 	 * Get resource
 	 * @return
-	 * @throws IOException 
 	 */
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getJs() throws IOException {
+	public String getJs() {
 		return "// ok";
 	}
 }
