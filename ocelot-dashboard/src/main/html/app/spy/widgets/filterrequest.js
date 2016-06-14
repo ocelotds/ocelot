e8727bce84b1d0dd3d1c0d16e8f95ceb622e1855
hhfrancois <francois.achache@gmail.com>
2016-06-14 21:40:02 +0200

    usearrays.filter

--- ocelot-dashboard/src/main/html/app/spy/widgets/filterrequest.js
+++ ocelot-dashboard/src/main/html/app/spy/widgets/filterrequest.js
@@ -2,17 +2,13 @@
 	'use strict';
 	angular.module('spy.module').filter('filterrequest', function () {
 		return function (input, filterInput, triggerDelay, showOnlyWarning) {
-			var result = [];
 			if(input) {
-				input.forEach(function (request, idx, arr) {
-					if((request.mfc.ds.search(filterInput) !== -1 || request.mfc.op.search(filterInput) !== -1) && 
-						(!showOnlyWarning || request.mtc.type === 'FAULT' || request.t > triggerDelay)) {
-						result.push(request);
-					}
+				return input.filter(function(request) {
+					return ((request.mfc.ds.search(filterInput) !== -1 || request.mfc.op.search(filterInput) !== -1) && 
+						(!showOnlyWarning || request.mtc.type === 'FAULT' || request.t > triggerDelay));
 				});
 			}
-			return result;
+			return input;
 		};
 	});
 })();
-
