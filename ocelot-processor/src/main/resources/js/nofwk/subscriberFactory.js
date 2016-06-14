adf3da0f40e967ed757028b6b0e25619b80120fa
hhfrancois <francois.achache@gmail.com>
2016-06-14 20:30:55 +0200

    add closure and add service to window

--- ocelot-processor/src/main/resources/js/nofwk/subscriberFactory.js
+++ ocelot-processor/src/main/resources/js/nofwk/subscriberFactory.js
@@ -1,20 +1,22 @@
-var subscriberFactory = (function () {
-	'use strict';
-	return {
-		createSubscriber:function(topic) {
-			return new Subscriber(topic);
-		}
-	};
+(function () {
+	window.subscriberFactory = (function () {
+		'use strict';
+		return {
+			createSubscriber:function(topic) {
+				return new Subscriber(topic);
+			}
+		};
+	})();
+	window.Subscriber = (function (topic) {
+		var promise = ocelotServices.subscribe(topic);
+		Object.defineProperty(promise, "topic", {value: topic, writable: false}); // add topic property, readonly
+		delete promise.constraint;
+		promise.unsubscribe = function () {
+			var p = ocelotServices.unsubscribe(topic);
+			p.uid = this.uid;
+			return p;
+		};
+		return promise;
+	});
 })();
-var Subscriber = (function (topic) {
-	var promise = ocelotServices.subscribe(topic);
-	Object.defineProperty(promise, "topic", {value: topic, writable: false}); // add topic property, readonly
-	delete promise.constraint;
-	promise.unsubscribe = function () {
-		var p = ocelotServices.unsubscribe(topic);
-		p.uid = this.uid;
-		return p;
-	};
-	return promise;
-});
 
