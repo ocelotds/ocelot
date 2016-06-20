664c7b7ab397e466b568a3ec5480b25ecbcc381c
hhfrancois <francois.achache@gmail.com>
2016-06-21 00:00:42 +0200

    import useless

--- ocelot-spi/src/main/java/org/ocelotds/annotations/JsTopic.java
+++ ocelot-spi/src/main/java/org/ocelotds/annotations/JsTopic.java
@@ -6,8 +6,8 @@ package org.ocelotds.annotations;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Retention;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
 import javax.enterprise.util.Nonbinding;
 import javax.interceptor.InterceptorBinding;
@@ -20,7 +20,7 @@ import javax.interceptor.InterceptorBinding;
  */
 @Inherited
 @InterceptorBinding
-@Retention(RUNTIME)
+@Retention(RetentionPolicy.RUNTIME)
 @Target({METHOD, TYPE})
 public @interface JsTopic {
 	@Nonbinding String value() default "";
